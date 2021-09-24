687d1131d22d20aa0716e82e60783ad3049dd6ab
Julien Chee <julienarrow@outlook.com>
2021-09-24 08:20:07 -0700

    update

---
+++ facebookscraper/spiders/facebooklikecount.py
@@ -0,0 +1,15 @@
+import scrapy
+
+class Facebooklikecount(scrapy.Spider):
+    name = 'facebookprofile'
+    allowed_domains = ['facebook.com']
+    start_urls = ["https://graph.facebook.com/{username}?access_token={access_token}&fields=likes"]
+
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0].format(username=self.username, access_token=self.access_token), self.parse))
+
+    def parse(self, response):
+        print('start=================================')
+        print(response)
+        print('end=================================')
+        
