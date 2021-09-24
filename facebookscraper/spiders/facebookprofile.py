0e61a072a35d721382c66c6ca59b0189c9d51590
Julien Chee <julienarrow@outlook.com>
2021-09-24 06:43:00 -0700

    update facebookprofile spider

--- facebookscraper/spiders/facebookprofile.py
+++ facebookscraper/spiders/facebookprofile.py
@@ -6,14 +6,19 @@ from scrapy.spiders import CrawlSpider, Rule
 class FacebookprofileSpider(CrawlSpider):
     name = 'facebookprofile'
     allowed_domains = ['facebook.com']
-    start_urls = ['http://facebook.com/']
-
+    start_urls = ['https://www.facebook.com/pg/{username}/community/?ref=page_internal']
+    HEADERS = {
+                'Accept-language': 'en\r\n',
+                'Cookie': 'foo=bar\r\n',
+                'User-Agent': 'Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n' 
+               }
     rules = (
         Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
         item = {}
+        print response
         #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
         #item['name'] = response.xpath('//div[@id="name"]').get()
         #item['description'] = response.xpath('//div[@id="description"]').get()
