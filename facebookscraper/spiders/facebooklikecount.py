bb915d1dcd945b1bdb7364cfd2ddca9427689190
Julien Chee <julienarrow@outlook.com>
2021-09-24 08:23:28 -0700

    fix issue

--- facebookscraper/spiders/facebooklikecount.py
+++ facebookscraper/spiders/facebooklikecount.py
@@ -1,12 +1,12 @@
 import scrapy
 
 class Facebooklikecount(scrapy.Spider):
-    name = 'facebookprofile'
+    name = 'facebooklikecount'
     allowed_domains = ['facebook.com']
     start_urls = ["https://graph.facebook.com/{username}?access_token={access_token}&fields=likes"]
 
     def start_requests(self):
-        yield scrapy.Request(self.start_urls[0].format(username=self.username, access_token=self.access_token), self.parse))
+        yield scrapy.Request(self.start_urls[0].format(username=self.username, access_token=self.access_token), self.parse)
 
     def parse(self, response):
         print('start=================================')
