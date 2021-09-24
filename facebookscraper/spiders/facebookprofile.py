2a11c97fb35e0985ec6286bcaeb1c65029e78f7f
Julien Chee <julienarrow@outlook.com>
2021-09-24 07:51:40 -0700

    update

--- facebookscraper/spiders/facebookprofile.py
+++ facebookscraper/spiders/facebookprofile.py
@@ -8,10 +8,13 @@ class FacebookprofileSpider(CrawlSpider):
     allowed_domains = ['facebook.com']
     start_urls = ["https://www.facebook.com/pg/{username}/community/?ref=page_internal"]
     HEADERS = {
-                'Accept-language': 'en\r\n',
-                'Cookie': 'foo=bar\r\n',
-                'User-Agent': 'Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n' 
-               }
+        'Accept-language': 'en\r\n',
+        'Cookie': 'foo=bar\r\n',
+        'User-Agent': 'Mozilla/5.0 (iPad; U; CPU OS 3_2 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Version/4.0.4 Mobile/7B334b Safari/531.21.102011-10-16 20:23:10\r\n' 
+    }
+    custom_settings = {
+        'ROBOTSTXT_OBEY': False
+    }
     rules = (
         Rule(LinkExtractor(deny=()), callback='parse_item', follow=True),
     )
